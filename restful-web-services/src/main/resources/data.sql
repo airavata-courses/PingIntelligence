d5e0e0c7c98dcaa85b3770ca21b7241b8aeb86d3
Sudip Padhye <sudip.padhye@gmail.com>
2021-03-05 23:34:46 -0500

    Dockerizing Uploading service & updating README

--- restful-web-services/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO todo(id, username, description, target_date, is_done) 
-values(10001, 'user', 'Learn JPA', sysdate(), false);
-
-INSERT INTO todo(id, username, description, target_date, is_done) 
-values(10002, 'user', 'Learn Data JPA', sysdate(), false);
-
-INSERT INTO todo(id, username, description, target_date, is_done) 
-values(10003, 'user', 'Learn Micro-JPA', sysdate(), false);
---
+++ uploadgoogledrive/readme.md
@@ -0,0 +1,10 @@
+<p align="center">
+  <img width="280" height="200"
+  src="https://github.com/airavata-courses/PingIntelligence/blob/main/Design_Documents/Logo/logo.JPG">
+</p>
+
+## Steps to run
+
+> docker build -f Dockerfile -t upload-google-image .
+
+> docker run -p 9092:9092 upload-google-image
