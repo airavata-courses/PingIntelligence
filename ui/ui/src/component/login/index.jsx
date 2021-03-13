34e3f328c51984cc3596c56854d6f51744a6baa1
Sudip Padhye <sudip.padhye@gmail.com>
2021-03-13 12:15:33 -0500

    CLeaning old files

--- ui/ui/src/component/login/index.jsx
+++
@@ -1,5 +0,0 @@
-import { findRenderedComponentWithType } from "react-dom/test-utils";
-
-export {Login} from "./login";
-export {Register} from "./register";
-export {Dashboard} from "../home/dashboard";
