05e15e8797209e4c9b1de1ef562daaa6aa9f8d45
Sudip Padhye <sudip.padhye@gmail.com>
2021-03-18 11:34:03 -0400

    Syncing branch with the latest microservice code

--- gateway.js
+++ gateway.js
@@ -14,6 +14,7 @@ const FormData = require("form-data");
 const fs = require("fs");
 const qs = require("querystring");
 var request = require("request");
+const Blob = require("cross-blob");
 
 app.use(fileUpload());
 app.use(express.urlencoded({ extended: true }));
@@ -27,7 +28,7 @@ const formidable = require("formidable");
 app.post("/login", (req, res) => {
   console.log(req.body);
   axios
-    .post("http://localhost:8080/user/login", {
+    .post("http://user-mgmt:8091/user/login", {
       username: req.body.username,
       password: req.body.password,
     })
@@ -46,7 +47,7 @@ app.post("/login", (req, res) => {
 app.post("/register", (req, res) => {
   console.log(req.body);
   axios
-    .post("http://localhost:8080/user/signup", {
+    .post("http://user-mgmt:8091/user/signup", {
       username: req.body.username,
       password: req.body.password,
       firstname: req.body.firstname,
@@ -70,7 +71,7 @@ app.post("/verifyusertoken", (req, res) => {
   // console.log(req.body)
   axios
     .post(
-      "http://localhost:8080/user/verify",
+      "http://user-mgmt:8091/user/verify",
       qs.stringify({
         secret: "Bearer " + req.body.secret,
       }),
@@ -95,7 +96,7 @@ app.post("/verifyusertoken", (req, res) => {
 app.post("/createalbum", (req, res) => {
   console.log(req.body);
   axios
-    .post("http://localhost:8090/album/create", {
+    .post("http://upload-mgmt:8092/album/create", {
       name: req.body.name,
       description: req.body.description,
       firstname: req.body.firstname,
@@ -117,7 +118,7 @@ app.post("/createalbum", (req, res) => {
 app.post("/useralbums", (req, res) => {
   // console.log(req.body)
   axios
-    .get("http://localhost:8090/album/allalbums?username=" + req.body.username)
+    .get("http://upload-mgmt:8092/album/allalbums?username=" + req.body.username)
     .then(function (response) {
       console.log(response.data);
       res.send(response.data);
@@ -132,36 +133,48 @@ app.post("/useralbums", (req, res) => {
  */
 
 app.post("/uploadalbumphotos", (req, res) => {
+
   const formData = new FormData();
   const data = req.files.files;
-  // console.log(data)
-  var i;
+  console.log(data)
+  const photo =  req.body.photoname.split(",")
+
   for (i = 0; i < data.length; i++) {
-    fs.writeFileSync("./testfile1.jpg", data.data, (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-    });
-    // console.log(req.body.sharedusers)
 
-    var options = {
-      method: "POST",
-      url: "http://localhost:8090/album/upload",
-      headers: {
-        "cache-control": "no-cache",
-        "content-type": "multipart/form-data",
-      },
-      formData: {
-        albumname: req.body.albumname,
-        sharedusers: req.body.sharedusers,
-        photoname: req.body.photoname,
-        files: fs.createReadStream("testfile1.jpg"),
-      },
-    };
+    fs.writeFileSync(
+      "./assets/" + photo[i] + ".jpg",
+      data[i].data.toString("base64"),
+      function (err) {}
+    );
+    
+  var options = {
+    method: "POST",
+    url: "http://upload-mgmt:8092/album/upload",
+    headers: {
+      "cache-control": "no-cache",
+      "content-type": "multipart/form-data",
+    },
+    formData: {
+      albumname: req.body.albumname,
+      sharedusers: req.body.sharedusers,
+      photoname: photo[i],
+      files: fs.readFileSync(
+        "./assets/" + photo[i] + ".jpg",
+        function (err, data) {
+          if (!err) {
+          } else {
+            // console.log(err);
+          }
+        }
+      ),
+    },
+  };
 
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-    });
-  }
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  });
+}
+
   res.send("200");
 });
 
@@ -172,7 +185,7 @@ app.post("/uploadalbumphotos", (req, res) => {
 app.post("/sharealbum", (req, res) => {
   var options = {
     method: "POST",
-    url: "http://localhost:8090/album/share",
+    url: "http://upload-mgmt:8092/album/share",
     headers: {
       "cache-control": "no-cache",
       "content-type": "application/json",
@@ -194,13 +207,13 @@ app.post("/sharealbum", (req, res) => {
 app.post("/findonealbum", (req, res) => {
   var options = {
     method: "GET",
-    url: "http://localhost:8090/album/locatealbum?albumname=" +
-    req.body.albumname 
+    url:
+      "http://upload-mgmt:8092/album/locatealbum?albumname=" + req.body.albumname,
   };
-  console.log(options)
+  console.log(options);
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
-    res.send(body)
+    res.send(body);
   });
 });
 
@@ -211,29 +224,95 @@ app.post("/findonealbum", (req, res) => {
 app.post("/searchphotometadata", (req, res) => {
   var options = {
     method: "GET",
-    url: "http://127.0.0.1:8000/search/images/",
+    url: "http://metadata-search:8000/search/images/",
     headers: {
       "cache-control": "no-cache",
       "content-type": "application/json",
     },
     qs: {
-      height: req.query.height,
-      width: req.query.width,
-      mode: req.query.mode,
-      formats: req.query.format,
-      image: req.query.image,
-      annotationtags: req.query.annotationtags,
-      created_on: req.query.created_on,
-      description: req.query.description,
-      photoname: req.query.photoname,
+      height: req.body.height,
+      width: req.body.width,
+      mode: req.body.mode,
+      formats: req.body.format,
+      image: req.body.image,
+      annotationtags: req.body.annotationtags,
+      created_on: req.body.created_on,
+      description: req.body.description,
+      title: req.query.photoname,
     },
     json: true,
   };
   request(options, function (error, response, body, callback) {
-    if (error) throw new Error(error);
+    // if (error) throw new Error(error);
     res.send(body);
   });
 });
+
+/**
+ * open one photos
+ */
+
+// app.post("/viewphoto", (req, res) => {
+//   console.log("hello")
+//   var options = {
+//     method: "GET",
+//     url: "http://127.0.0.1:8000/google/upload/images/?title" + req.body.photoname
+//   };
+//   request(options, function (error, response, body) {
+//       console.log(body)
+//       res.send(new Buffer.from(body,"base64"))
+//   });
+
+//   });
+
+app.post("/downloadimage", (req, res) => {
+  console.log("hello");
+  console.log("idsssssssssssssss" + req.body.ids);
+  var options = {
+    method: "GET",
+    url: "http://upload-mgmt:8092/photo/download?ids=" + req.body.ids,
+  };
+  console.log(options);
+  request(options, function (error, response, body) {
+    console.log(body);
+    res.send(JSON.stringify(body));
+    // res.send(new Buffer.from(body,"base64"))
+  });
+});
+
+
+app.post("/deletealbum", (req, res) => {
+  console.log(req.body);
+  axios
+    .post("http://upload-mgmt:8092/album/delete", {
+      albumname: req.body.albumname
+    })
+    .then(function (response) {
+      // console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      // console.log(error);
+    });
+});
+
+app.post("/deletephoto", (req, res) => {
+  console.log(req.body);
+  axios
+    .post("http://upload-mgmt:8092/photo/delete", {
+      photo_id: req.body.ids,
+      albumname: req.body.albumname
+    })
+    .then(function (response) {
+      // console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      // console.log(error);
+    });
+});
+
+
 
 app.use(express.json());
 app.listen(PORT, () => {
