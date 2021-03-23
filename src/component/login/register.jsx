fcc199883cd418f80285d20b44533efa41ef6f67
danagar0312 <49617042+danagar0312@users.noreply.github.com>
2021-03-23 00:10:48 -0400

    Update register.jsx

--- src/component/login/register.jsx
+++ src/component/login/register.jsx
@@ -26,7 +26,7 @@ export class Register extends React.Component{
     }
     
     handleRegister(ev){
-        var targetUrl = "http://149.165.170.179:gateway/register";
+        var targetUrl = "http://gateway:3001/register";
         const requestOptions = {
         method: "POST",
         headers: {'Content-Type': 'application/json', Accept: 'application/json'},
