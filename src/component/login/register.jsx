018186ff968d065cf0a9bb6c051ec22a049dd4a1
deepika awasthi <danagar0312@gmail.com>
2021-03-23 21:48:36 -0400

    adding dns

--- src/component/login/register.jsx
+++ src/component/login/register.jsx
@@ -26,7 +26,7 @@ export class Register extends React.Component{
     }
     
     handleRegister(ev){
-        var targetUrl = "http://149.165.170.179:30002/register";
+        var targetUrl = "http://gateway:30002/register";
         const requestOptions = {
         method: "POST",
         headers: {'Content-Type': 'application/json', Accept: 'application/json'},
