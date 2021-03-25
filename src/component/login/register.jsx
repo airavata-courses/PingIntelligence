9d74cbdcdd493c51b43351d5a26a8598b7d010d6
Sudip Padhye <sudip.padhye@gmail.com>
2021-03-25 16:15:02 -0400

    Update register.jsx

--- src/component/login/register.jsx
+++ src/component/login/register.jsx
@@ -26,8 +26,9 @@ export class Register extends React.Component{
     }
     
     handleRegister(ev){
- //       var targetUrl = "http://localhost:3001/register";
-	var targetUrl = "http://149.165.156.145:30001/register";
+	//var targetUrl = "http://gateway:3001/register";
+        var targetUrl = "http://gateway.default:3001/register";
+	//var targetUrl = "http://149.165.156.145:30001/register";
 
         const requestOptions = {
         method: "POST",
