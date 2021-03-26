36b71811f74c58aa8bffcc90109cdb1040e186e9
Sudip Padhye <sudip.padhye@gmail.com>
2021-03-26 13:04:41 -0400

    Update KafkaPublisherConfig.java

--- src/main/java/com/iu/photosquad/mainuploadmgmt/mainuploadmgmt/config/KafkaPublisherConfig.java
+++ src/main/java/com/iu/photosquad/mainuploadmgmt/mainuploadmgmt/config/KafkaPublisherConfig.java
@@ -21,7 +21,7 @@ public class KafkaPublisherConfig {
 	@Bean
 	public ProducerFactory<String,KafkaAlbumModel> producerFactory(){
 			Map<String,Object> config = new HashMap<>();
-	        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+	        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-mgmt:9092");
 	        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 	        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 //	        config.put(ProducerConfig.FETCH_MAX_BYTES_CONFIG, "41943040");
