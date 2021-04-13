f80d82d5adebd427cb5d0e2351374b4a49f7c995
Sudip Padhye <sudip.padhye@gmail.com>
2021-04-13 19:37:59 -0400

    Initial Commit

--- ui/src/component/login/index.jsx
+++
@@ -1,8 +0,0 @@
-import { findRenderedComponentWithType } from "react-dom/test-utils";
-
-export {Login} from "./login";
-export {Register} from "./register";
-export {Dashboard} from "../home/dashboard";
-export {Viewphotos} from "../photo/album_photos";
-export {PhotoSearchModal} from "../photo/photo_search.jsx";
-export {Logout} from './logout';
